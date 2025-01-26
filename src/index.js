10d0de88ad78ea754be4a6b9eb2454ec23cbe5aa
shi-v-ani-singh <shivanisingh1408203@gmail.com>
2025-01-26 10:07:04 +0530

    check

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {Provider} from "react-redux";
 import store from "./utils/store"
 
 
-const root = RextDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
